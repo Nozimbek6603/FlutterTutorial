628d55a9279f2bd8e50565e7984f78d96749b65b
nozimbek6603 <nozimbek6603@gmail.com>
2024-09-13 20:29:35 +0500

    Scaffold widget qisqacha

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello world"),
+      home: Scaffold(
+    backgroundColor: Colors.blue[200],
+    appBar: AppBar(
+      title: Text("lesson3"),
+      backgroundColor: Colors.blue,
     ),
-  ));
+    body: Center(
+      child: Text("This is body"),
+    ),
+  )));
 }
