19a174f4baecac515f991a9de0780e0775525d47
nozimbek6603 <nozimbek6603@gmail.com>
2024-09-13 23:36:53 +0500

    Container, margin, padding tushunchalari

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,21 @@ void main() {
   runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.blue[200],
-          appBar: AppBar(
-            title: Text("lesson3"),
-            backgroundColor: Colors.blue,
+          backgroundColor: Colors.indigo,
+          body: SafeArea(
+            child: Container(
+              height: 100,
+              width: 100,
+              // margin: EdgeInsets.all(10),
+              // margin: EdgeInsets.only(left: 30),
+              margin: EdgeInsets.fromLTRB(10,20,30,40),
+              padding: EdgeInsets.all(30),
+              // padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+              child: Text("Hello"),
+              color: Colors.green,
+
+            ),
           ),
-          body: Image(
-            image: AssetImage("images/NB_group.jpg"),
-          )
         )
       )
   );
