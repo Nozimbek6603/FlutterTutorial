d479cc244385df1b572ca2c5e8c0d945f68864f6
nozimbek6603 <nozimbek6603@gmail.com>
2024-10-12 15:44:34 +0500

    switch and floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
-
-void main(){
+void main() {
   // WidgetsFlutterBinding.ensureInitialized();
   // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-
 
   runApp(MyApp());
 }
@@ -19,59 +17,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "FlutterDemo",
-      theme: ThemeData(primarySwatch: Colors.blue, visualDensity: VisualDensity.adaptivePlatformDensity),
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isSwitchOn = false;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final isorientation = MediaQuery.of(context).orientation==Orientation.portrait;
-
+    final isorientation =
+        MediaQuery.of(context).orientation == Orientation.portrait;
 
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text("App"),),
-      body: isorientation ? Container(
-        child: Column(
-          children: [
-            Container(
-              height: size.height*0.5,
-              color: Colors.yellow,
-              margin: EdgeInsets.all(20),
+        title: Text("App"),
+      ),
+      body: isorientation
+          ? Container(
+              child: Column(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Switch(
+                      value: isSwitchOn,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitchOn = value;
+                        });
+                      },
+                    ),
+                  ]),
+                  Container(
+                    height: size.height * 0.5,
+                    color: Colors.yellow,
+                    margin: EdgeInsets.all(20),
+                  ),
+                  Container(
+                    height: size.height * 0.2,
+                    color: Colors.yellow,
+                    margin: EdgeInsets.all(20),
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              child: Row(
+                children: [
+                  Container(
+                    width: size.height * 0.2,
+                    color: Colors.red,
+                    margin: EdgeInsets.all(20),
+                  ),
+                  Container(
+                    width: size.height * 0.5,
+                    margin: EdgeInsets.all(20),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
             ),
-            Container(
-              height: size.height*0.2,
-              color: Colors.yellow,
-              margin: EdgeInsets.all(20),
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
             ),
-          ],
-        ),
-      )
-      :Container(
-        child: Row(
-          children: [
-            Container(
-              width: size.height*0.2,
-              color: Colors.red,
-              margin: EdgeInsets.all(20),
-            ),
-            Container(
-              width: size.height*0.5,
-              margin: EdgeInsets.all(20),
-
-              color: Colors.blue,
-            ),
-          ],
-        ),
-      )
     );
   }
 }
